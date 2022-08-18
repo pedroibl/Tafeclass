a82d8fcf4991163a34926054295fbf067f8595ef
pedroibl <pedroivolima0@gmail.com>
2022-08-18 10:23:21 +0000

    g

---
+++ sati_class/print_format.py
@@ -0,0 +1,57 @@
+print("name")
+
+
+print("year = 2016")
+
+
+print('The story of {0}, {1}, and {other}.'.format('Bill', 'Manfred', other='Georg'))
+
+print('I love {} on mine "{}!"'.format('Pizza', 'Dinner'))
+
+
+print("John ordered a Pepperoni Pizza for $4.25 with a 15% tip")
+
+
+print("customer_name ordered a order_name for price with a tip_percentage tip")
+
+print('%n')
+
+print("Lorem %s dolor sit %s..." % ("ipsum", "amet"))
+
+print("Apples can be {}, {}, and {}.".format("red", "yellow", "green"))
+
+
+print("{:~^10}".format("TEST"))
+
+
+print("{:~<10}".format("TEST"))
+
+print("{:~>10}".format("TEST"))
+
+print("{:~=10}".format(-58))
+
+
+print("{:~^10}".format("TEST"))
+
+print("{:+} {:+}".format(58, -58))
+
+
+print("{:-} {:-}".format(58, -58))
+
+
+print("{: } {: }".format(58, -58))
+
+print("{0:#b} {0:#o} {0:#x} {0:#X}".format(124))
+
+print("{:~^10}".format("TEST"))
+
+print("{:_}".format(123456789))
+
+
+print("{:_x}".format(123456789))
+
+print("{:,}".format(123456789))
+
+
+print("{:.3f}".format(5 / 3))
+
